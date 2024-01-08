9433bfb7c972671703e53993d1374f0bf822503e
Elena <helenochka4@gmail.com>
2024-01-08 20:32:33 +0100

    move DB_HOST to config.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const DB_HOST= "mongodb+srv://Olena:hiaDpFnTUDQl4l7c@cluster1.dr5ntwt.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const {DB_HOST} = require('./config');
+
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOST)
