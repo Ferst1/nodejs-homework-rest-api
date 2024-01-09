2152e6b5d0edb664848a86072c1969bf6172b8a9
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-01-09 13:53:09 +0100

    test

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const {DB_HOST,PORT=3000} = process.env;
-
+// const {DB_HOST,PORT=3000} = process.env;
+const DB_HOST = "mongodb+srv://Olena:hiaDpFnTUDQl4l7c@cluster1.dr5ntwt.mongodb.net/db-contacts?retryWrites=true&w=majority"
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOST)
 .then(()=> {
-  app.listen(PORT)
+  app.listen(PORT=3000, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`);
+  });
   })
 .catch(error => {
   console.log(error.message);
