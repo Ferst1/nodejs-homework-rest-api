1f4fc0b013695b20f6b651840b58dae1f21c0096
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-09 17:55:24 +0100

    add env

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const {DB_HOST,PORT=3000} = process.env;
-
+const {DB_HOST} = process.env;
 
 
 mongoose.set('strictQuery', true);
 mongoose.connect(DB_HOST)
 .then(()=> {
-  app.listen(PORT=3000, () => {
-    console.log(`Server running. Use our API on port: ${PORT}`);
-  });
+  app.listen(3000)  
   })
+  
 .catch(error => {
   console.log(error.message);
   process.exit(1);
