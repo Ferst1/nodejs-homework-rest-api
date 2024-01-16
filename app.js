a013364abe2e757fa6e0be9128f92f35a93405b5
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-16 17:21:32 +0100

    users modal

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+
+const authRouter = require("./routes/api/auth");
+
+
+
+
 const app = express();
 const contactsRouter = require("./routes/api/contacts");
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -10,6 +16,9 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/auth", authRouter);
 
 app.use("/api/contacts", contactsRouter);
 
---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const router = express.Router();
+//signup
+router.post("/register")
+
+module.exports = router;
