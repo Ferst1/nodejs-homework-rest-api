c5db0305a5d8bee3ede2cfa50026aeab29795155
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-16 19:33:27 +0100

    bcrypt

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+const bcrypt = require("bcrypt");
+const createHashPassword = async (password) => {
+  // const salt = await bcrypt.genSalt(10);
+  // console.log(salt);
+
+  const result = await bcrypt.hash(password, 10);
+  // console.log(result);
+  const compareResult1 = await bcrypt.compare(password, result);
+
+  console.log(compareResult1);
+  const compareResult2 = await bcrypt.compare("123456", result);
+  console.log(compareResult2);
+}
+createHashPassword('123456')
+
+
+
 
 const authRouter = require("./routes/api/auth");
 
