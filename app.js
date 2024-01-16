9e3f683eb23106320f185fa38c44d47961294e86
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-16 19:05:59 +0100

    registration user

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
+
+const authRouter = require("./routes/api/auth");
+
 const app = express();
 const contactsRouter = require("./routes/api/contacts");
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -11,6 +14,8 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
