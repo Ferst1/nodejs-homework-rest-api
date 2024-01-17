a8a9cd7611c1613c82b9429c530bfa617f8181bd
Elena <36508011+Ferst1@users.noreply.github.com>
2024-01-17 19:58:24 +0100

    new feld owner

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,21 +4,31 @@ const { User } = require("../models/user");
 const  HttpError  = require("../helpers/HttpError");
 
 const { SECRET_KEY } = process.env;
+
+
+
 const authenticate = async (req, res, next) => {
     const { authorization = "" } = req.headers;
-    const [bearer, token] = authorization.split(' ');
+    const [bearer, token] = authorization.split(' ',2);
     
     if (bearer !== 'Bearer') {
-         next(new HttpError(401));
+         next(new HttpError(401,"Invalid token"));
+    }
+
+
+    req.user ={
+        id:decode.id,
+        name:decode.name,
     }
 
     try {
-        const { id } = jwt.verify(token, SECRET_KEY);
+        const { id } = jwt.verify(token,SECRET_KEY);
         const user = await User.findById(id);
         
-        if (!user) {
-             next(new HttpError(401));
-        }
+        if (!user || !user.token || user.token !== token) {
+			next(HttpError(401, "Invalid token"));
+		}
+		req.user = user;
         
         next();
     } 
